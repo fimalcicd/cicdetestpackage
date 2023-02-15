a7e304d7bf3eb2e72cdc4ab75e8ceadce359c838
InUnity <inunity@InUnitys-Mac-mini.local>
2023-02-15 14:03:01 +0530

    test one

---
+++ lib/cicdtestpackage.dart
@@ -0,0 +1,16 @@
+library cicdtestpackage;
+
+import 'package:flutter/material.dart';
+
+class CiCdTestWidget extends StatelessWidget {
+  const CiCdTestWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      height: 100,
+      width: 100,
+    );
+  }
+}
